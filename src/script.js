6d146bb54451bde87647af842a2a09d3a588488e
Ray Ikome <raycikome@gmail.com>
2023-07-06 11:52:43 -0400

    added permutation size input

--- src/script.js
+++ src/script.js
@@ -1,27 +1,29 @@
 const getPermutations = document.querySelector(".get-permutations")
 const outputDiv = document.querySelector(".results-list")
 const inputNums = document.querySelector(".numbers")
+const permutationSizeInput = document.querySelector(".permutation-size")
 
-function convert(num, arr) {
+function convert(num, arr, permSize) {
     let results = []
-    for(i = 0; i < 3; i++){
+    for(i = 0; i < permSize; i++){
         results.push(parseInt(arr[num % arr.length]))
         num = parseInt(num / arr.length);
     }
     return results.join("");
 }
 
-function permute(nums) {
+function permute(nums, permSize) {
     let results = []
 
     for (var i = 0; i < parseInt(Math.pow(nums.length, 3)); i++) {
-        results.push(convert(i, nums))
+        results.push(convert(i, nums, permSize))
     }
     return results
 };
 
 getPermutations.addEventListener("click", () => {
-    let permutations = permute(`${inputNums.value}`.split(""))
+    console.log(permutationSizeInput.value)
+    let permutations = permute(`${inputNums.value}`.split(""), permutationSizeInput.value)
     console.log(permutations)
     outputDiv.textContent = ""
     permutations.forEach(e => {
